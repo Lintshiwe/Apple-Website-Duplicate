cc3d239a4cbc9e6f5a150249b2bc344283e645e1
lintshiwe <ntoampilp@gmail.com>
2026-06-22 16:38:40 +0200

    double nav link right padding to pr-28

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -203,7 +203,7 @@ export default function Navbar() {
                 href={item.href}
                 onMouseEnter={() => handleNavEnter(item.label)}
                 onMouseLeave={handleNavLeave}
-                className={`text-xs transition-colors no-underline pr-14 ${
+                className={`text-xs transition-colors no-underline pr-28 ${
                   activeDropdown === item.label
                     ? "text-black"
                     : "text-neutral-600 hover:text-neutral-900"
